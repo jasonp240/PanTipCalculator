02a79427b85d4b8c1f67645956008fbe13473ecf
jason <jpan5985@bths.edu>
2023-09-21 16:52:08 -0400

    Added a chart for average typing percents

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -12,14 +12,21 @@ public class TipCalculator {
         double tipPerPerson;
         double costPerPersonWithTip;
         List<String> foodList = new ArrayList<>();
-
-
         // gathering basic info
         Scanner scan = new Scanner(System.in); // initializing scanner
         System.out.println("Welcome to the tip calculator!");
         System.out.print("How many people are in your group: ");
         int numPeople = scan.nextInt();
         scan.nextLine();
+        // information for tipping
+        System.out.println("-------------------------------");
+        System.out.println("|    Service    | Typical Tip |");
+        System.out.println("|   Restaurant  |    10-25%   |");
+        System.out.println("|   Bartenders  |    15-20%   |");
+        System.out.println("|  Room Service |    15-20%   |");
+        System.out.println("|     Barber    |    10-20%   |");
+        System.out.println("|      Taxi     |    15-20%   |");
+        System.out.println("-------------------------------"); // separating sections
 
         System.out.print("What's the tip percentage? (0-100): ");
         int tipPercentage = scan.nextInt();
@@ -42,17 +49,14 @@ public class TipCalculator {
                 foodList.add(scan.nextLine());
             }
         }
-
-        System.out.println("-------------------------------"); // separating sections
-
         //calculations
         totalTip = Math.round(100 * (totalBill * (.01 * tipPercentage))) / 100.0;
         totalBillWithTip = Math.round(100 * (totalBill + totalTip)) / 100.0;
         personCost = Math.round(100 * (totalBill / numPeople)) / 100.0;
         tipPerPerson = Math.round(100 * (totalTip / numPeople)) / 100.0;
         costPerPersonWithTip = Math.round(100 * (totalBillWithTip / numPeople)) / 100.0;
-
         // printing out info
+        System.out.println("-------------------------------"); // separating sections
         System.out.println("Total bill before tip: $" + totalBill);
         System.out.println("Total percentage: " + tipPercentage + "%");
         System.out.println("Total tip: $" + totalTip);
@@ -60,9 +64,8 @@ public class TipCalculator {
         System.out.println("Per person cost before tip: $" + personCost);
         System.out.println("Tip per person: $" + tipPerPerson);
         System.out.println("Total cost per person: $" + costPerPersonWithTip);
-
         System.out.println("-------------------------------"); // separating sections
-
+        // for each loop to print out each individual food
         for (String x : foodList) {
             System.out.println(x);
             scan.close();
