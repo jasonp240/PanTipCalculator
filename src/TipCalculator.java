71c7428faeea1ccf0e8ad7e3dec4f721e4943793
BT_1N3_16 <sample@bths.edu>
2023-09-21 09:00:21 -0400

    Added calculations for the bill (tip + cost per person)

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -4,6 +4,12 @@ public class TipCalculator {
     public static void main(String[] args) {
         // initializing variables
         double totalBill = 0.0;
+        double totalTip;
+        double totalBillWithTip;
+        double personCost;
+        double tipPerPerson;
+        double costPerPerson;
+
 
         // gathering basic info
         Scanner scan = new Scanner(System.in); // initializing scanner
@@ -20,11 +26,23 @@ public class TipCalculator {
         double foodCost = scan.nextDouble();
         scan.nextLine();
 
+        // initializing while loop
         while (foodCost != -1) {
-            totalBill += foodCost;
+            totalBill += foodCost; // adding cost to the bill (won't include -1)
             System.out.print("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
             foodCost = scan.nextDouble();
+            scan.nextLine();
         }
+
+        System.out.print("-------------------------------");
+
+        //calculations
+        totalTip = totalBill * (.01 * tipPercentage);
+        totalBillWithTip = totalBill + totalTip;
+        personCost = totalBill / numPeople;
+        tipPerPerson = totalTip / numPeople;
+        costPerPerson = totalBillWithTip / numPeople;
+        
         scan.close();
     }
 }
